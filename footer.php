914c7b7132511c6a7392f24ae96749d0269a92f7
MariusPaq <marius.paquet.com>
2020-05-25 16:22:33 +0200

    footer comments

--- footer.php
+++ footer.php
@@ -1,15 +1,14 @@
     <div class="footer">
+      <div class="topfooter"><!--footer(blog,links,tweets,contact)-->
 
-      <div class="topfooter">
-
-          <div class="box">
+          <div class="box"><!--Blog(p)-->
             <h4 class="tiltefooter">From The Blog</h4>
             <h5 id="posttitle">Post title</h5>
             <p class="pfoot">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet ultrices eros. Nullam lobortis nibh diam, vitae maximus elit iaculis id. Pellentesque non mollis nulla. Quisque sit amet vehicula eros. Pellentesque non turpis iaculis, rhoncus mi at, laoreet enim. Integer sapien velit, viverra a tortor sed, efficitur blandit erat. Donec quis condimentum odio, feugiat tristique nulla.</p>
             <a href="#">Read More</a>
-          </div>
+          </div><!--FIN Box-->
 
-          <div class="box">
+          <div class="box"><!--Links(a)-->
             <h4>Quick Links</h4>
             <ul class="liste">
               <li>
@@ -34,18 +33,17 @@
                 <a href="#">Sed a nulla urna</a>
               </li>
             </ul>
-          </div>
+          </div><!--FIN Box-->
 
-          <div class="box">
+          <div class="box"><!--tweets(p)-->
             <h4>Latest Tweets</h4>
             <p class="pfoot">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet ultrices eros. Nullam lobortis nibh diam, vitae maximus elit iaculis id. Pellentesque non mollis nulla. Quisque sit amet vehicula eros.</p>
             <p class="pfoot">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet ultrices eros. Nullam lobortis nibh diam, vitae maximus elit iaculis id. Pellentesque non mollis nulla. Quisque sit amet vehicula eros.</p>
-          </div>
+          </div><!--FIN Box-->
 
-          <div class="box">
+          <div class="box"><!--Contact(Formulaire)-->
             <h4>Contact Us</h4>
-            <!--formulaire-->
-            <form action="/ma-page-de-traitement" method="post">
+            <form action="/ma-page-de-traitement" method="post"><!--Formulaire 4 label(name,email,subject,message)-->
               <div class="label">
                 <label for="name"></label>
                 <input type="text" id="name" name="user_name" value="Full Name">
@@ -65,25 +63,20 @@
               <div class="label">
                 <button type="submit">Submit</button>
               </div>
-            </form>
-          </div>
+            </form><!--FIN Formulaire-->
+          </div><!--FIN Box-->
 
-      </div>
-
-      <div class="barrenoir">
-
-      </div>
-
-      <div class="belowfooter">
+      </div><!--FIN topheader-->
+      <div class="barrenoir"></div><!--barre noire entre topfooter & belowheader-->
+      <div class="belowfooter"><!--Copyright(p)-->
         <p id="copyright">Copyright © 2020 Marius Paquet - All rights Reserved</p>
         <p id="copyright2">Intégration n-1</p>
-      </div>
+      </div><!--FIN belowheader-->
 
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
   </body>
 </html>
