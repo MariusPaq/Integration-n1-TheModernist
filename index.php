7e0e5f9679fe0526a87fec3aaa29f1b984c9fcc6
MariusPaq <marius.paquet.com>
2020-05-25 16:55:24 +0200

    index comments

--- index.php
+++ index.php
@@ -1,123 +1,104 @@
 <?php include 'header.php' ?>
-
-    <div class="latestwork">
-
+  <div class="latestwork"><!--index content-->
     <div class="inlatestwork">
 
-      <div class="toplatestwork">
+      <div class="toplatestwork"><!--contenue du haut de l'index-->
+        <div class="flexcard"><!--card(img,h3,p)-->
 
-        <div class="flexcard">
-
-
-            <div class="card" id="card1">
+            <div class="card" id="card1"><!--card1-->
               <a href="#">
-                <img src="img/icone1.png" class="card-img-top" alt="...">
+              <img src="img/icone1.png" class="card-img-top" alt="...">
               </a>
                 <div class="card-body">
                   <h3>Card Title</h3>
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
+                </div><!--FIN card-body-->
+            </div><!--FIN card-->
 
-          <div class="card">
+          <div class="card"><!--card2-->
             <a href="#">
             <img src="img/icone2.png" class="card-img-top" alt="...">
             </a>
             <div class="card-body">
               <h3>Card Title</h3>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
+            </div><!--FIN card-body-->
+          </div><!--FIN card-->
 
-          <div class="card">
+          <div class="card"><!--card3-->
             <a href="#">
             <img src="img/icone3.png" class="card-img-top" alt="...">
             </a>
             <div class="card-body">
               <h3>Card Title</h3>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
+            </div><!--FIN card-body-->
+          </div><!--FIN card-->
 
-          <div class="card" id="card4">
+          <div class="card" id="card4"><!--card4-->
             <a href="#">
-              <img src="img/icone4.png" class="card-img-top" alt="...">
-              </a>
-              <div class="card-body">
-                <h3>Card Title</h3>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-          </div>
+            <img src="img/icone4.png" class="card-img-top" alt="...">
+            </a>
+            <div class="card-body">
+              <h3>Card Title</h3>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div><!--FIN card-body-->
+          </div><!--FIN card-->
 
-        </div>
-
+        </div><!--FIN flexcard-->
+      </div><!--FIN toplatestwork-->
+      <div class="barrework">
+        <h3>LATEST WORK</h3>
       </div>
-
-        <div class="barrework">
-          <h3>LATEST WORK</h3>
-        </div>
-
-
-      <div class="belowlatestwork">
-
+      <div class="belowlatestwork"><!--1er ligne des box-->
         <div class="boxwork" id="boxwork1">
           <div class="topbox">
-
           </div>
           <div class="titrebox">
             <h5>LOREM IPSUM DOLOR</h5>
           </div>
-        </div>
-
+        </div><!--FIN boxwork1-->
         <div class="boxwork" id="boxwork2">
           <div class="topbox">
-
           </div>
           <div class="titrebox">
             <h5>LOREM IPSUM DOLOR</h5>
           </div>
-        </div>
-
+        </div><!--FIN boxwork2-->
         <div class="boxwork" id="boxwork3">
           <div class="topbox">
-
           </div>
           <div class="titrebox">
             <h5>LOREM IPSUM DOLOR</h5>
           </div>
-        </div>
+        </div><!--FIN boxwork3-->
+      </div><!--FIN belowlatestwork-->
 
-      </div>
-
-      <div class="belowlatestwork2">
+      <div class="belowlatestwork2"><!--2em ligne des box-->
         <div class="boxwork" id="boxwork4">
           <div class="topbox">
-
           </div>
           <div class="titrebox">
             <h5>LOREM IPSUM DOLOR</h5>
           </div>
-        </div>
-
+        </div><!--FIN boxwork4-->
         <div class="boxwork" id="boxwork5">
           <div class="topbox">
-
           </div>
           <div class="titrebox">
             <h5>LOREM IPSUM DOLOR</h5>
           </div>
-        </div>
-
+        </div><!--FIN boxwork5-->
         <div class="boxwork" id="boxwork6">
           <div class="topbox">
-
           </div>
           <div class="titrebox">
             <h5>LOREM IPSUM DOLOR</h5>
           </div>
-        </div>
-      </div>
-    </div>
-    </div>
+        </div><!--FIN boxwork6-->
+      </div><!--FIN belowlatestwork2-->
+
+    </div><!--FIN inlatestwork-->
+  </div><!--FIN latestwork-->
 
 <?php include 'footer.php' ?>
